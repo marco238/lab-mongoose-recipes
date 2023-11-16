61a07d6ea886769dd01c8d986f5c390a252730fa
Marcos Monzón <marcos.monzon.iparraguirre.ext@ing.com>
2023-11-16 11:01:10 +0100

    add comments

--- index_2.js
+++ index_2.js
@@ -17,14 +17,22 @@ mongoose
     // Antes de hacer nada, borramos todas las recetas que pueda haber ya en la base de datos
     Recipe.deleteMany()
       .then(() => {
+        // Con el método Recipe.create() creamos todas las recetas en la base de datos de una vez
         Recipe.create(data)
           .then(() => {
+            // Con el método Recipe.findOneAndUpdate() actualizamos la duración de la receta 'Rigatoni alla Genovese'
+            // El primer parámetro es el filtro (qué receta queremos actualizar)
+            // El segundo parámetro es el cambio que queremos hacer
             Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, { duration: 100 })
               .then(() => {
+                // Con el método Recipe.deleteOne() borramos la receta 'Carrot Cake'
+                // El primer parámetro es el filtro (qué receta queremos borrar)
                 Recipe.deleteOne({ title: 'Carrot Cake' })
                   .then(() => {
+                    // Cerramos la conexión a la base de datos
                     mongoose.connection.close()
                       .then(() => {
+                        // Si todo ha ido bien, mostramos un mensaje
                         console.log('Connection closed');
                       })
                       .catch(error => {
